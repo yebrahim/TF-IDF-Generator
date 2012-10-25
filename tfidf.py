d12fea7fe245603ade63746c92fa8ca875fccf73
Yasser <yasser.ebrahim@gmail.com>
2012-10-25 03:48:24 +0200

    More proper header
    deleted tmp, now called input_files.txt

--- tfidf.py
+++ tfidf.py
@@ -1,3 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+File: tfidf.py
+Author: Yasser Ebrahim
+Date: Oct 2012
+
+Generate the TF-IDF ratings for a collection of documents.
+
+This script will also tokenize the input files to extract words (removes punctuation and puts all in
+    lower case), and it will use the NLTK library to lemmatize words (get rid of stemmings)
+
+IMPORTANT:
+    A REQUIRED library for this script is NLTK, please make sure it's installed along with the wordnet
+    corpus before trying to run this script
+
+Usage:
+    - Create a file to hold the paths+names of all your documents (in the example shown: input_files.txt)
+    - Make sure you have the full paths to the files listed in the file above each on a separate line
+    - For now, the documents are only collections of text, no HTML, XML, RDF, or any other format
+    - Simply run this script file with your input file as a single parameter, for example:
+            python tfidf.py input_files.txt
+    - This script will generate new files, one for each of the input files, with the prefix "tfidf_"
+            which contains terms with corresponding tfidf score, each on a separate line
+
+"""
+
 import sys, re, math
 from nltk.stem.wordnet import WordNetLemmatizer
 
